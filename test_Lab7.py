20df111d47372b5eafcac6be5c97ab222abac82e
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2024-05-04 22:52:37 -0400

    Initial Commit

---
+++ test_Lab7.py
@@ -0,0 +1,27 @@
+# Tests for Lab5
+# Falling Distance Calculator
+
+import os.path
+import sys
+from Lab7 import main
+from tud_tests import *
+
+def test_Lab7():
+
+    try:
+        exists = os.path.exists("Lab7.py")
+        assert exists == True
+    except:
+        sys.exit()
+
+    # Test 1
+    set_keyboard_input(["zebra","}heud"])
+    main()
+    output = get_display_output()
+
+    assert output == [
+        "Enter the message to be encrypted: ",
+        "Encrypted message = }heud\n",
+        "Enter the cipher to be decrypted: ",
+        "Decrypted message = zebra\n"
+        ]
